04f831086d4f5d03a100a4610b25ccae7040508a
miwi31 <miwi31@VM01.myguest.virtualbox.org>
2025-02-10 23:32:21 +0500

    add progression game files and corresponding changes

---
+++ src/games/progression.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+const brainProgression = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log('What number is missing in the progression?');
+
+  let correctAnswer = 0;
+  
+  for (let i = 1; i <= 3; i += 1) {
+    let firstNumber = Math.floor(100 * Math.random());
+    let diff = Math.floor(31 * Math.random());
+    const progression = [];
+    const progressionLength = 10;
+  
+    for (let i = 0; i < progressionLength; i += 1) {
+      progression.push(firstNumber + (i * diff));
+    }
+
+    const indexOfHiddenNumber = Math.floor(progressionLength * Math.random());
+    correctAnswer = progression[indexOfHiddenNumber];
+    progression[indexOfHiddenNumber] = '..';
+  
+    console.log(`Question: ${progression.join(' ')}`);
+    let answer = readlineSync.question('Your answer: ');
+  
+    if (answer === correctAnswer.toString()) {
+      console.log(`Correct!`);
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${userName}!`);
+    }
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default brainProgression;
