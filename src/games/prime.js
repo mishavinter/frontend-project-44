0c23a882faf9b3ca5f94d19d66701c96ca91bfbb
miwi31 <miwi31@VM01.myguest.virtualbox.org>
2025-02-23 03:26:10 +0500

    fix logics error in PRIME-game

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,13 +7,19 @@ let correctAnswer = 'yes';
 const generateGameData = () => {
   const question = Math.floor(200 * Math.random());
 
-  let i = 2;
-  while (i <= question / 2) {
-    if (question % i === 0) {
-      correctAnswer = 'no';
-      i += 200;
+  if (question === 0) {
+    correctAnswer = 'no';
+  } else if (question <= 3) {
+    correctAnswer = 'yes';
+  } else {
+    let i = 2;
+    while (i <= question / 2) {
+      if (question % i === 0) {
+        correctAnswer = 'no';
+        i += 200;
+      }
+      i += 1;
     }
-    i += 1;
   }
 
   return [question, correctAnswer];
