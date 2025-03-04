7a7d318a371b12cbd89191316ed541402a07889f
miwi31 <miwi31@VM01.myguest.virtualbox.org>
2025-03-05 00:11:07 +0500

    reorganize logics in gcd-game

--- src/games/isGCD.js
+++ src/games/isGCD.js
@@ -1,4 +1,22 @@
-import { mainEngine, getRandomNumber, getGCD } from '../index.js';
+import { mainEngine, getRandomNumber } from '../index.js';
+
+const getGCD = (num1, num2) => {
+  const minNumber = Math.min(num1, num2);
+  const maxNumber = Math.max(num1, num2);
+  let GCD = 1;
+  if (minNumber === 0) {
+    GCD = maxNumber;
+  } else if (maxNumber % minNumber === 0) {
+    GCD = minNumber;
+  } else {
+    for (let i = 1; i <= minNumber / 2; i += 1) {
+      if (num1 % i === 0 && num2 % i === 0) {
+        GCD = i;
+      }
+    }
+  }
+  return GCD.toString();
+};
 
 const description = 'Find the greatest common divisor of given numbers.';
 
